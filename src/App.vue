17e264e9f8b1d891af46d592b2d1ab96ce6cb98f
Kevin McCarthy <mccarthyk@hcflgov.net>
2019-08-16 14:45:02 -0400

    rework without map, add statistic component

--- src/App.vue
+++ src/App.vue
@@ -1,129 +1,14 @@
-<template lang="html">
-  <div class="row">
-
-    <div class="col-lg-7">
-      <SurveyMap class="mb-3" />
-    </div>
-
-    <div class="col-lg-5">
-
-      <!-- Nav tabs -->
-      <ul class="nav nav-tabs" role="tablist">
-        <li role="presentation" :class="isTabActive('average')">
-          <a href="#average" @click="activeSlot = 'average'" class="px-3 py-2">Average</a>
-        </li>
-        <li role="presentation" :class="isTabActive('district1')">
-          <a href="#district1" @click="activeSlot = 'district1'" class="px-3 py-2">District 1</a>
-        </li>
-        <li role="presentation" :class="isTabActive('district2')">
-          <a href="#district2" @click="activeSlot = 'district2'" class="px-3 py-2">2</a>
-        </li>
-        <li role="presentation" :class="isTabActive('district3')">
-          <a href="#district3" @click="activeSlot = 'district3'" class="px-3 py-2">3</a>
-        </li>
-        <li role="presentation" :class="isTabActive('district4')">
-          <a href="#district4" @click="activeSlot = 'district4'" class="px-3 py-2">4</a>
-        </li>
-      </ul>
-
-      <transition name="fade" mode="out-in">
-        <div class="my-2 surevey-result-slot" :key="activeSlot">
-          <slot :name="activeSlot"></slot>
-        </div>
-      </transition>
-
-    </div>
-
-  </div>
-</template>
-
 <script>
-import SurveyMap from './components/SurveyMap'
+import Statistic from './components/Statistic'
 
 export default {
   name: 'Hc2019CountywideSurvey',
   install (Vue) {
     Vue.mixin({
       components: {
-        Hc2019CountywideSurvey: this
+        SurveyStatistic: Statistic
       }
     })
-  },
-  components: {
-    SurveyMap
-  },
-  data: () => ({
-    activeSlot: 'average'
-  }),
-  methods: {
-    isTabActive (tab) {
-      return tab == this.activeSlot ? 'active' : null
-    }
   }
 }
 </script>
-
-<style media="screen">
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-
-.surevey-result-slot ul {
-  list-style: none;
-  padding-left: 1rem;
-}
-
-.surevey-result-slot li {
-  font-size: 1.25em;
-  margin: 1rem 0;
-  font-style: italic;
-  color: #193D5C;
-}
-
-.survey-result-percent {
-  background: #ff0;
-  display: inline-block;
-  min-width: 10px;
-  padding: 5px 7px;
-  /* font-size: 12px; */
-  /* font-weight: 700; */
-  line-height: 1.5;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  border-radius: 50%;
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-  color: #193D5C;
-  font-style: normal;
-}
-
-.surevey-result-slot li:nth-child(1) .survey-result-percent {
-  background: #A4A4A6;
-  background: linear-gradient(rgba(164,164,166, 1), rgba(164,164,166, 0.5));
-}
-.surevey-result-slot li:nth-child(2) .survey-result-percent {
-  background: #F6B5AB;
-  background: linear-gradient(rgba(246,181,171, 1), rgba(246,181,171, 0.5));
-}
-.surevey-result-slot li:nth-child(3) .survey-result-percent {
-  background: #EA7461;
-  background: linear-gradient(rgba(234,116,97, 1), rgba(234,116,97, 0.5));
-}
-.surevey-result-slot li:nth-child(4) .survey-result-percent {
-  background: #7EC9CE;
-  background: linear-gradient(rgba(126,201,206, 1), rgba(126,201,206, 0.5));
-}
-.surevey-result-slot li:nth-child(5) .survey-result-percent {
-  color: #fff;
-  background: #0D4368;
-  background: linear-gradient(rgba(13,67,104, 1), rgba(13,67,104, 0.5));
-}
-.surevey-result-slot li:nth-child(6) .survey-result-percent {
-  background: #A8A9AC;
-  background: linear-gradient(rgba(168,169,172, 1), rgba(168,169,172, 0.5));
-}
-</style>
---
+++ src/components/Statistic.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+  <div class="survey-statistic lead my-4">
+
+    <hr>
+
+    <div class="row d-lg-flex align-items-center">
+      <div class="col-lg-4 svg-container">
+        <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut img-fluid img-responsive">
+          <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#ddd" stroke-width="2"></circle>
+
+          <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" :stroke="chartColor" stroke-width="3" :stroke-dasharray="dashArray" stroke-dashoffset="25"></circle>
+
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size=".4em" font-weight="bold" font-family="Arial, Helvetica, sans-serif">{{ percent }}%</text>
+        </svg>
+      </div>
+      <div class="col-lg-8">
+
+        <slot></slot>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    percent: {
+      type: String,
+      required: true
+    },
+    chartColor: {
+      type: String,
+      default: '#EA7461'
+    }
+  },
+  computed: {
+    inverse () {
+      return 100 - parseInt(this.percent)
+    },
+    dashArray () {
+      return `${this.percent} ${this.inverse}`
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.survey-statistic:nth-child(even) .svg-container {
+  order: 12;
+}
+</style>
