9ebe9b1f76aa029d016fe31ee4148d23c3ebe37c
Kevin McCarthy <mccarthyk@hcflgov.net>
2019-08-19 12:27:42 -0400

    adjust responsive grid size for statistic elements

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -3,8 +3,8 @@
 
     <hr>
 
-    <div class="row d-lg-flex align-items-center">
-      <div class="col-lg-4 svg-container">
+    <div class="row d-flex align-items-center">
+      <div class="col-xs-4 svg-container">
         <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut img-fluid img-responsive">
           <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#ddd" stroke-width="2"></circle>
 
@@ -13,7 +13,7 @@
           <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size=".4em" font-weight="bold" font-family="Arial, Helvetica, sans-serif">{{ percent }}%</text>
         </svg>
       </div>
-      <div class="col-lg-8">
+      <div class="col-xs-8">
 
         <slot></slot>
 
